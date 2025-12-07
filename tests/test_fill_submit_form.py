dfe8666008bd2b61b9011f9da13ddd52840d8691
Alena Panova <aljonchik1905@mail.ru>
2025-12-07 22:08:31 +0800

    add test demoqa for lesson 11

---
+++ tests/test_fill_submit_form.py
@@ -0,0 +1,34 @@
+import os
+import time
+from datetime import date
+
+import allure
+
+from qa_guru_homework_11.registration_steps import RegistrationSteps
+from qa_guru_homework_11.user import User
+
+@allure.title("Проверка регистрации пользователя")
+@allure.epic("DEMOQA")
+@allure.feature("Форма регистрации")
+def test_fill_submit_form(setup_browser):
+    student = User(
+        first_name='Alex',
+        last_name='Bagel',
+        email='alexbagel@mail.ru',
+        gender='Male',
+        mobile_number='9021778990',
+        date_of_birth=date(1990, 6, 19),
+        subject='English',
+        hobbies='Sports',
+        picture=os.path.join(os.path.dirname(os.path.dirname(__file__)),'mount.jpg'),
+        street_address='Lomonosov str. 8',
+        state_address='Haryana',
+        city_address='Panipat'
+    )
+
+    registration_steps = RegistrationSteps()
+
+    registration_steps.open()
+    registration_steps.register(student)
+    registration_steps.should_have_registered(student)
+
