2108e00c3963d0700af2f30291298d19648645d8
Alena Panova <aljonchik1905@mail.ru>
2025-12-07 23:19:56 +0800

    change conftest for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,38 @@
-import pytest
-from selene import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import sys
 import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
 
 @pytest.fixture(scope='function')
-def setup_browser():
-    chrome_options = Options()
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "127.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    # Определяем, запущены ли в CI (Jenkins)
-    is_ci = os.getenv('JENKINS_HOME') is not None
+    yield driver
 
-    # Основные опции для скрытия автоматизации
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
+    attach.add_screenshot(driver)
+    attach.add_logs(driver)
+    attach.add_html(driver)
+    #attach.add_video(driver)
 
-    # Дополнительные опции
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-extensions")
-
-    if is_ci:
-        # Для Jenkins/CI - headless режим
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("--window-size=1920,1080")
-    else:
-        # Для локального запуска
-        chrome_options.add_argument("--start-maximized")
-
-    # ВАЖНО: Создаем драйвер с опциями
-    browser.config.driver = webdriver.Chrome(options=chrome_options)
-    browser.config.base_url = "https://demoqa.com"  # Исправьте на ваш URL
-
-    # Скрипты для маскировки (раскомментируйте если нужно)
-    # browser.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-
-    yield browser
-
-    # Очистка
-    if browser.driver:
-        browser.quit()
+    driver.quit()
