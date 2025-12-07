8ca60a918d5d13be8e6ecd87afa18fed29275520
Alena Panova <aljonchik1905@mail.ru>
2025-12-07 22:49:35 +0800

    change conftest for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,16 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
 
 
 @pytest.fixture(scope='function')
 def setup_browser():
     chrome_options = Options()
+
+    # Определяем, запущены ли в CI (Jenkins)
+    is_ci = os.getenv('JENKINS_HOME') is not None
 
     # Основные опции для скрытия автоматизации
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
@@ -16,13 +21,24 @@ def setup_browser():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--start-maximized")
 
-    # Конфигурация Selene
-    browser.config.driver_options = chrome_options
-    browser.config.base_url = "https://example.com"  # опционально
+    if is_ci:
+        # Для Jenkins/CI - headless режим
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--window-size=1920,1080")
+    else:
+        # Для локального запуска
+        chrome_options.add_argument("--start-maximized")
 
-    # Скрипты для маскировки
-    #browser.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    # ВАЖНО: Создаем драйвер с опциями
+    browser.config.driver = webdriver.Chrome(options=chrome_options)
+    browser.config.base_url = "https://demoqa.com"  # Исправьте на ваш URL
+
+    # Скрипты для маскировки (раскомментируйте если нужно)
+    # browser.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
     yield browser
-    browser.quit()
+
+    # Очистка
+    if browser.driver:
+        browser.quit()
